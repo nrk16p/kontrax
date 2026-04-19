c94121c12943e2304abc8de791a4a9bf999a37f9
Menatransport_02 <menatransport_02@Menatransport-02s-MacBook-Air.local>
2026-04-19 09:09:12 +0700

    fixed: index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import './index.css';
 import React from "react";
 import { render } from "react-dom";
-import { App } from "./App";
+import App from "./App";   // ✅ แก้ตรงนี้
+
 render(<App />, document.getElementById("root"));
