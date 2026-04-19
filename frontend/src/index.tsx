38e52f5cb657788bc57901dbb7bd6601264374e6
psompong-127 <psompong.biz@gmail.com>
2026-04-19 21:36:37 +0700

    Import firebase library in index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,5 +2,6 @@ import './index.css';
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";   // ✅ แก้ตรงนี้
+import "./lib/firebase"; // add firebase
 
 render(<App />, document.getElementById("root"));
